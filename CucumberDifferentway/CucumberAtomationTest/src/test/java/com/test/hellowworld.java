c4db0b4778885787c2b86bf1f6baf65d48a81aab
pahlabi <pahlabi@DESKTOP-283DCKO.lan>
2020-04-04 22:58:16 -0400

    commit

--- CucumberDifferentway/CucumberAtomationTest/src/test/java/com/test/hellowworld.java
+++ CucumberDifferentway/CucumberAtomationTest/src/test/java/com/test/hellowworld.java
@@ -5,6 +5,11 @@ public class hellowworld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hellow world");
+		System.out.println("Hellow world");
+		System.out.println("Hellow world");
+		System.out.println("Hellow world");
+		System.out.println("Hellow world");
+		System.out.println("Hellow world");
 	}
 
 }
